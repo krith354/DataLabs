fb97d633b89c0bdb780efc4b01527185d09e00b3
Cristian Ruiz <cristian.ruiz1@upr.edu>
2018-03-03 23:31:23 -0400

    Merging labs to one repo

---
+++ DataLab2/InheritanceLabStudents/src/orderedStructures/Fibonacci.java
@@ -0,0 +1,31 @@
+package orderedStructures;
+
+public class Fibonacci extends Progression {
+
+	private double prev; 
+	
+	public Fibonacci() { 
+		this(1); 
+		prev = 0; 
+	}
+	private Fibonacci(double first) {
+		super(first);
+	}
+
+	@Override
+	public double nextValue() {
+        // add the necessary code here
+//		System.out.println(current + "," + prev + "," + this.firstValue());
+		current = prev + current;
+		prev = current - prev;
+		return current;
+	}
+	
+	@Override	
+	public double firstValue() { 
+		double value = super.firstValue(); 
+		prev = 0; 
+		return value; 
+	}
+
+}
---
+++ DataLab2/InheritanceLabStudents/src/orderedStructures/Geometric.java
@@ -0,0 +1,30 @@
+package orderedStructures;
+
+public class Geometric extends Progression {
+
+	private double commonFactor; 
+	
+	public Geometric(double firstValue, double commonFactor) { 
+		super(firstValue); 
+		this.commonFactor = commonFactor; 
+	}
+	
+	@Override
+	public double nextValue() {
+		current = current * commonFactor; 
+		return current;
+	}
+	
+	@Override
+	public String toString() {
+		return "Geom(" + (int)this.firstValue() + "," + (int)this.commonFactor + ")"; 
+	}
+	
+	public double getTerm(int n) throws IndexOutOfBoundsException { 
+		if (n <= 0) 
+			throw new IndexOutOfBoundsException("printAllTerms: Invalid argument value = " + n); 
+		double value = (this.firstValue())*Math.pow(this.commonFactor, n-1);
+		return value;
+	}
+
+}
