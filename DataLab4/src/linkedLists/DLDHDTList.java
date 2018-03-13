c72f58da14ec77b68b807c3aef776d6f46506b33
Cristian Ruiz <cristian.ruiz1@upr.edu>
2018-03-12 23:51:18 -0400

    Missing Exercise 6 from Lab 4

--- DataLab4/src/linkedLists/DLDHDTList.java
+++ DataLab4/src/linkedLists/DLDHDTList.java
@@ -1,5 +1,6 @@
 package linkedLists;
 
+import java.lang.reflect.Array;
 import java.util.NoSuchElementException;
 
 public class DLDHDTList<E> extends AbstractDLList<E> {
@@ -38,6 +39,14 @@ public class DLDHDTList<E> extends AbstractDLList<E> {
 
 	public void addNodeBefore(Node<E> target, Node<E> nuevo) {
 		// ADD CODE HERE
+		DNode<E> dnuevo = (DNode<E>) nuevo;
+		DNode<E> nAfter =  (DNode<E>) target;
+		DNode<E> nBefore = nAfter.getPrev();
+		nBefore.setPrev(dnuevo); 
+		nAfter.setNext(dnuevo); 
+		dnuevo.setNext(nBefore); 
+		dnuevo.setPrev(nAfter); 
+		length++; 
 	}
 
 	public Node<E> createNewNode() {
@@ -58,14 +67,18 @@ public class DLDHDTList<E> extends AbstractDLList<E> {
 
 	public Node<E> getNodeAfter(Node<E> target)
 			throws NoSuchElementException {
-		// ADD CODE HERE AND MODIFY RETURN ACCORDINGLY
-		return null; 
+		if(this.getLastNode() == target)
+			throw new NoSuchElementException("target is the last node");
+		DNode<E> dNode = ((DNode<E>) target).getNext();
+		return dNode; 
 	}
 
 	public Node<E> getNodeBefore(Node<E> target)
 			throws NoSuchElementException {
-		// ADD CODE HERE AND MODIFY RETURN ACCORDINGLY
-		return null; 
+		if(this.getFirstNode() == target)
+			throw new NoSuchElementException("target is the first node, before is the dummy header node");
+		DNode<E> dNode = ((DNode<E>) target).getPrev();
+		return dNode; 
 	}
 
 	public int length() {
@@ -74,6 +87,19 @@ public class DLDHDTList<E> extends AbstractDLList<E> {
 
 	public void removeNode(Node<E> target) {
 		// ADD CODE HERE to disconnect target from the linked list, reduce lent, clean target...
+		//PRE: target is a data node (never dummy)
+		   DNode<E> dTarget = (DNode<E>) target; 
+		   //Get surrounding nodes in the linked list....
+		   //They exist! since at least the dummies are
+		   // always there....
+		   DNode<E> prevNode = dTarget.getPrev(); 
+		   DNode<E> nextNode = dTarget.getNext(); 
+		   // Properly set the links of those surrounding nodes
+		   prevNode.setNext(nextNode); 
+		   nextNode.setPrev(prevNode); 
+		   // now clean all fields in the removed node.
+		   dTarget.clean();
+		   length--;
 	}
 	
 	/**
@@ -111,5 +137,33 @@ public class DLDHDTList<E> extends AbstractDLList<E> {
 	        super.finalize();
 	    }
 	}
+	
+	// The following two methods are to be implemented as part of an exercise
+		public Object[] toArray() {
+			Object[] arr = new Object[this.length()];
+			DNode<E> dNode = (DNode<E>) this.getFirstNode();
+			int i = 0;
+			while(dNode.getNext() != null)
+				arr[i] = dNode.getNext().getElement();
+				i++;
+			return arr;
+		}
+
+		public <T1> T1[] toArray(T1[] array) {
+			// TODO as in Exercise 3
+			if(array.length < this.length()) {
+				array = (T1[]) Array.newInstance(array.getClass().getComponentType(), this.length());
+			} else if(array.length > this.length()) {
+				for(int i=this.length();i<array.length;i++) {
+					array[i] = null;
+				}
+			}
+			DNode<E> dNode = (DNode<E>) this.getFirstNode();
+			int i = 0;
+			while(dNode.getNext() != null)
+				array[i] = (T1) dNode.getNext().getElement();
+				i++;
+			return array;
+		}
 
 }
